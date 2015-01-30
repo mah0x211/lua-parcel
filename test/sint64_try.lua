94902a0366ca55ef99ad422a5afbdc5123349ffa
Masatoshi Teruya <mah0x211@gmail.com>
2015-01-30 17:23:11 +0900

    add: test/sint64_try.lua has been added.

---
+++ test/sint64_try.lua
@@ -0,0 +1,17 @@
+local pack = require('parcel.pack');
+local unpack = require('parcel.unpack');
+local bin;
+
+-- 64 bit signed integer
+-- 8bit type(0x87) + 64 bit value = 9 byte
+for _, v in ipairs({ -9223372036854775808, -2147483649 }) do
+    bin = ifNil( pack( v ) );
+    -- check size
+    ifNotEqual( #bin, 9 );
+    ifNotEqual( unpack( bin ), v );
+end
+
+-- check boundary values
+-- num > -2147483649
+bin = ifNil( pack( -2147483648 ) );
+ifNotEqual( #bin, 5 );
