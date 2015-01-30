133f0498d5fc0d72fd6ce88c36b96c9cfe4fbe0b
Masatoshi Teruya <mah0x211@gmail.com>
2015-01-30 18:01:27 +0900

    add: test/float64_try.lua has been added.

---
+++ test/float64_try.lua
@@ -0,0 +1,12 @@
+local pack = require('parcel.pack');
+local unpack = require('parcel.unpack');
+local bin;
+
+-- 64 bit float(double)
+-- 8bit type(0xA3) + 64 bit value = 9 byte
+for _, v in ipairs({ 0.1, 1.7976931348623158E+308, 2.2204460492503131E-16 }) do
+    bin = ifNil( pack( v ) );
+    -- check size
+    ifNotEqual( #bin, 9 );
+    ifNotEqual( unpack( bin ), v );
+end
