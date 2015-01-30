2305d0f28933da1782d16b4818cf0cc9cbedef7d
Masatoshi Teruya <mah0x211@gmail.com>
2015-01-30 18:28:22 +0900

    add: test/str8_try.lua has been added.

---
+++ test/str8_try.lua
@@ -0,0 +1,33 @@
+local pack = require('parcel.pack');
+local unpack = require('parcel.unpack');
+local bin, v;
+
+local function genStr( len )
+    local str = {};
+    
+    for i = 1, len do
+        str[i] = 'x';
+    end
+    
+    return table.concat( str );
+end
+
+-- 8 bit length string
+-- 8bit type(0x8C) + 8 bit length value + string value
+for i = 32, 255 do
+    v = genStr( i );
+    bin = ifNil( pack( v ) );
+    -- check size
+    ifNotEqual( #bin, 2 + #v );
+    ifNotEqual( unpack( bin ), v );
+end
+
+-- check boundary values
+-- str length < 32
+v = genStr( 31 );
+bin = ifNil( pack( v ) );
+ifNotEqual( #bin, 1 + #v );
+-- str length > 255
+v = genStr( 256 );
+bin = ifNil( pack( v ) );
+ifNotEqual( #bin, 3 + #v );
